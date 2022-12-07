b65d8021afb22ac36404112d5c9601dc5b2b283d
Vlad Kovac <currently.unkwn@gmail.com>
2022-12-07 12:46:09 +0200

    feat: Add react-glider initial setup

--- src/components/ProjectPreview/ProjectPreview.js
+++ src/components/ProjectPreview/ProjectPreview.js
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 
+import Glider from 'react-glider';
+import 'glider-js/glider.min.css';
+
 const ProjectPreview = ({ projectItem }) => {
   const { name, title, images } = projectItem;
 
@@ -9,11 +12,16 @@ const ProjectPreview = ({ projectItem }) => {
     <div>
       <Link to={projectLink}>{name}</Link>
       <div>
-        {/* {images
+        <Glider draggable hasArrows slidesToShow={1.5} slidesToScroll={1}>
+        {images
           .filter((_, index) => index < 4)
           .map((image) => (
-            <img src={image} alt="" />
-          ))} */}
+            <div><img src={image} alt="" /></div>
+          ))}
+          <div><Link to={projectLink}>View Work</Link></div>
+          </Glider>
+          
+          
       </div>
     </div>
   );
