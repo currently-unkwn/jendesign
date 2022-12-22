373b9c017a31ff3180e2bd5f95bb09846e77862c
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2022-12-22 12:11:39 +0200

    fix: Fix preloader not being global

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "../Header";
 import Home from "../../routes/Home";
@@ -6,17 +7,41 @@ import Contacts from "../../routes/Contacts";
 import DynamicBg from "../DynamicBg/DynamicBg";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const preloader = document.querySelector(".preloader");
+
+    const onPageLoad = () => {
+      // After page has loaded wait 1sec, then animate
+      setTimeout(() => {
+        preloader.classList.add("animated");
+        setLoading(false);
+      }, 1000);
+    };
+
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
+
   return (
-    <>
-      <DynamicBg />
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="projects/*" element={<Projects />} />
-          <Route path="contacts" element={<Contacts />} />
-        </Route>
-      </Routes>
-    </>
+    !loading && (
+      <>
+        <DynamicBg />
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home loading={loading} />} />
+            <Route path="projects/*" element={<Projects />} />
+            <Route path="contacts" element={<Contacts />} />
+          </Route>
+        </Routes>
+      </>
+    )
   );
 };
 
--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -8,9 +8,7 @@ import DynamicBgSection from "../../components/DynamicBgSection/DynamicBgSection
 
 import { ABOUT_DATA } from "../../data";
 
-const Home = () => {
-  const [loading, setLoading] = useState(true);
-
+const Home = ({ loading }) => {
   const heroRef = useRef();
   const projectsRef = useRef();
   const contactRef = useRef();
@@ -19,26 +17,6 @@ const Home = () => {
   const refsArray = [heroRef, projectsRef, contactRef, footerRef];
 
   const { id, text, image } = ABOUT_DATA;
-
-  useEffect(() => {
-    const preloader = document.querySelector(".preloader");
-
-    const onPageLoad = () => {
-      // After page has loaded wait 1sec, then animate
-      setTimeout(() => {
-        preloader.classList.add("animated");
-        setLoading(false);
-      }, 1000);
-    };
-
-    if (document.readyState === "complete") {
-      onPageLoad();
-    } else {
-      window.addEventListener("load", onPageLoad);
-
-      return () => window.removeEventListener("load", onPageLoad);
-    }
-  }, []);
 
   useEffect(() => {
     if (!loading) {
@@ -65,33 +43,31 @@ const Home = () => {
   }, [loading]);
 
   return (
-    !loading && (
-      <>
-        <main>
-          <MaxWidthWrapper>
-            <DynamicBgSection bgColor="red" ref={heroRef}>
-              <HeroSection>
-                <h1>{text}</h1>
-                <img src={image.src} alt={image.alt} />
-              </HeroSection>
-            </DynamicBgSection>
-          </MaxWidthWrapper>
-          <DynamicBgSection bgColor="blue" ref={projectsRef}>
-            <ProjectsSection />
-          </DynamicBgSection>
-          <MaxWidthWrapper>
-            <DynamicBgSection bgColor="lavender" ref={contactRef}>
-              <section>Get in Touch</section>
-            </DynamicBgSection>
-          </MaxWidthWrapper>
-        </main>
+    <>
+      <main>
         <MaxWidthWrapper>
-          <DynamicBgSection bgColor="cyan" ref={footerRef}>
-            <footer>Footer</footer>
+          <DynamicBgSection bgColor="red" ref={heroRef}>
+            <HeroSection>
+              <h1>{text}</h1>
+              <img src={image.src} alt={image.alt} />
+            </HeroSection>
           </DynamicBgSection>
         </MaxWidthWrapper>
-      </>
-    )
+        <DynamicBgSection bgColor="blue" ref={projectsRef}>
+          <ProjectsSection />
+        </DynamicBgSection>
+        <MaxWidthWrapper>
+          <DynamicBgSection bgColor="lavender" ref={contactRef}>
+            <section>Get in Touch</section>
+          </DynamicBgSection>
+        </MaxWidthWrapper>
+      </main>
+      <MaxWidthWrapper>
+        <DynamicBgSection bgColor="cyan" ref={footerRef}>
+          <footer>Footer</footer>
+        </DynamicBgSection>
+      </MaxWidthWrapper>
+    </>
   );
 };
 
